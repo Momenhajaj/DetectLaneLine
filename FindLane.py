4961a72b2feeb78eacddb0834d12b70b896b69ac
Mo'men hassan <momen.hajaj@gmail.com>
2017-11-21 16:26:25 +0200

    #Momenhagag

--- FindLane.py
+++ FindLane.py
@@ -3,8 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 import cv2
-
-
+import math
 
 
 #reading image
@@ -13,3 +12,15 @@ print('This image is:', type(image), 'with dimensions:', image.shape)
 plt.imshow(image)
 # if you wanted to show a single color channel image
 # called 'gray', for example, call as plt.imshow(gray, cmap='gray')
+
+def grayscale(img):
+    """Applies the Grayscale transform
+    (assuming your grayscaled image is called 'gray')
+    you should call plt.imshow(gray, cmap='gray')"""
+    return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # Or use BGR2GRAY if you read an image with cv2.imread()
+    # return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+def canny(img, low_threshold, high_threshold):
+    """Applies the Canny transform"""
+    return cv2.Canny(img, low_threshold, high_threshold)
